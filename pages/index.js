10e3c39f9848f2e5e45ef09a6c40f4cd496ea70f
Alpri Else <alprielse@gmail.com>
2019-01-23 20:21:57 -0600

    Initial landing page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
-
 const Index = () => (
   <div>
-    <h1>Welcome to r|p 2019</h1>
+    <link rel="stylesheet" href="/static/stylesheets/styles.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"/>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <div>
+      <div className="container">
+        <main role="main" className="">
+          <img className="logo mb-3" src="/static/img/logo.png" />
+          <div className="text-center text-white">
+            <h2 className="brand-title">reflections <span className="seperator">|</span> projections 2019</h2>
+            <p className="brand-description">25 years of connecting students with industry</p>
+          </div>
+          <br/>
+          <div className="text-white text-center">
+            <h4 className="coming-soon">Coming Soon</h4>
+          </div>
+          <br/>
+          <div className="text-white text-center">
+            <p className="contact">Questions? Interested in sponsoring? Email us at someemail@illinois.edu</p>
+          </div>
+      </main>
+      </div>
+    </div>
   </div>
 )
+
 export default Index
