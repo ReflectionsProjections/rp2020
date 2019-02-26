1926ba2a2297260386590cec615ebdfed0747958
Alpri Else <alprielse@gmail.com>
2019-02-26 15:18:56 -0600

    Add sponsor doc

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Head from 'next/head'
+import CustomHead from '../components/CustomHead'
 import SVG from 'react-inlinesvg'
 import CountdownTimer from '../components/CountdownTimer'
 
@@ -16,31 +16,7 @@ class Index extends React.Component {
   render () {
     return (
       <div>
-        <Head>
-          <title>Reflections | Projections 2019</title>
-          <meta name="description" content="Reflections | Projections is a tech conference organized and run by students at the University of Illinois at Urbana-Champaign."/>
-          <meta name="viewport" content="width=device-width, initial-scale=1"/>
-
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossOrigin="anonymous"/>
-          <link rel="stylesheet" href="/static/stylesheets/styles.css"/>
-
-          <link rel="apple-touch-icon" sizes="57x57" href="/static/assets/icons/apple-icon-57x57.png"/>
-          <link rel="apple-touch-icon" sizes="60x60" href="/static/assets/icons/apple-icon-60x60.png"/>
-          <link rel="apple-touch-icon" sizes="72x72" href="/static/assets/icons/apple-icon-72x72.png"/>
-          <link rel="apple-touch-icon" sizes="76x76" href="/static/assets/icons/apple-icon-76x76.png"/>
-          <link rel="apple-touch-icon" sizes="114x114" href="/static/assets/icons/apple-icon-114x114.png"/>
-          <link rel="apple-touch-icon" sizes="120x120" href="/static/assets/icons/apple-icon-120x120.png"/>
-          <link rel="apple-touch-icon" sizes="144x144" href="/static/assets/icons/apple-icon-144x144.png"/>
-          <link rel="apple-touch-icon" sizes="152x152" href="/static/assets/icons/apple-icon-152x152.png"/>
-          <link rel="apple-touch-icon" sizes="180x180" href="/static/assets/icons/apple-icon-180x180.png"/>
-          <link rel="icon" type="image/png" sizes="192x192"  href="/static/assets/icons/android-icon-192x192.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/static/assets/icons/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="96x96" href="/static/assets/icons/favicon-96x96.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/static/assets/icons/favicon-16x16.png"/>
-          <link rel="manifest" href="/static/assets/icons/manifest.json"/>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css"/>
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous"/>
-        </Head>
+        <CustomHead />
         <div>
           <nav className="navbar bg-blue" id="rp-nav">
             <div className="left-nav">
---
+++ pages/sponsor.js
@@ -0,0 +1,13 @@
+import CustomHead from '../components/CustomHead'
+
+
+
+const Sponsor = () => (
+  <div>
+    <CustomHead/>
+    <link rel="stylesheet" href="../static/stylesheets/sponsor.css"/>
+    <iframe class="frame" src="/static/corporateletter2019.pdf"/>
+  </div>
+)
+
+export default Sponsor
