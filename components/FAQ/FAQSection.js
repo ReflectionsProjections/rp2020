e76be1139860663bfedccb714b8a743ac73bc4e5
Rishub Podar <rishubpodar@wirelessprv-10-193-63-166.near.illinois.edu>
2019-03-10 22:15:18 -0500

    Added axios

---
+++ components/FAQ/FAQSection.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './FAQ.scss'
+
+class FAQSection extends FAQ {
+  state = {
+    questions: []
+  }
+
+  componentDidMount() {
+    axios.get(`jsonfile`)
+      .then(res => {
+        const questions = res.data;
+        this.setState({ questions });
+      })
+  }
+
+
+
+  render() {
+      return(
+        <div>
+          <div className="faq-container faq-column-container">
+            {questions.map((question, answer) =>
+              <section className="faq-column">
+                <h2>{question}</h2>
+                <p>{answer}</p>
+              </section>
+            )}
+          </div>
+        </div>
+      );
+  }
+}   
+
+
+export default FAQSection;
--- components/FAQ/index.js
+++ components/FAQ/index.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FAQ.scss'
+import FAQSection from './FAQSection';
 
 class FAQ extends Component {
-    constructor() {
-      super();
-  
-      this.state = {
-      };
-    }
+  state = {
+    sections: []
+  }
 
-    render() {
-        return(
-          <section className="faq">
-            <SubFaq />
-          </section>
-        );
-    }
+  componentDidMount() {
+    axios.get()
+      .then(res => {
+        const sections = res.data;
+        this.setState({ sections });
+      })
+  }
+
+  render() {
+      return(
+        <section className="faq">
+          {this.state.sections.map((sectionName) =>
+            <section className={sectionName}>
+              <FAQSection className={sectionName} />
+            </section>
+          )}
+        </section>
+      );
+  }
 }   
 
 
