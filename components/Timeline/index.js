d0d2200aa66e41909e0995b4ed2ffe76b1880c84
Alpri Else <alprielse@gmail.com>
2019-03-22 00:56:07 -0500

    Implement static file load

--- components/Timeline/index.js
+++ components/Timeline/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import TimelineBlock from './TimelineBlock';
 
 import './Timeline.scss';
 
-const Timeline = () => (
-  <div>
-    <TimelineBlock title="In the Beginning" />
-  </div>
-);
+class Timeline extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: undefined
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/static/data/timeline.json').then(res => {
+      const events = res.data;
+      this.setState({ events });
+    });
+  }
+
+  render() {
+    // Utilize this.state.events here for your components
+    // console.log(this.state.events);
+    return <TimelineBlock title="In the Beginning" />;
+  }
+}
 
 export default Timeline;
