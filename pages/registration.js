c495f11dcf5c84182c2169db4aa9c2e6581c582f
Rishub Podar <rishubpodar@wirelessprv-10-193-63-166.near.illinois.edu>
2019-04-03 11:23:44 -0500

    Created page with basic labels and state:

--- pages/registration.js
+++ pages/registration.js
@@ -3,119 +3,110 @@ import { Form, Row } from 'react-bootstrap';
 // import './Registration.scss';
 
 class Registration extends Component {
-  // state = {
-  //   firstName: '',
-  //   lastName: '',
-  //   phoneNumber: '',
-  //   email: '',
-  //   gender: '',
-  //   studentType: '',
-  //   major: '',
-  //   school: '',
-  //   transportation: '',
-  //   shirtSize: '',
-  //   diet: '',
-  //   graduationClass: '',
-  //   jobInterest: '',
-  //   professionalInterest: '',
-  //   heardFrom: '',
-  //   rpInterest: ''
-  // };
+  state = {
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    gender: '',
+    studentType: '',
+    major: '',
+    school: '',
+    transportation: '',
+    shirtSize: '',
+    diet: '',
+    graduationClass: '',
+    jobInterest: '',
+    professionalInterest: '',
+    heardFrom: '',
+    rpInterest: ''
+  };
 
   componentDidMount() {
     return () => {};
   }
 
   render() {
-    // const {
-    //   firstName,
-    //   lastName,
-    //   phoneNumber,
-    //   email,
-    //   gender,
-    //   studentType,
-    //   major,
-    //   school,
-    //   transportation,
-    //   shirtSize,
-    //   diet,
-    //   graduationClass,
-    //   jobInterest,
-    //   professionalInterest,
-    //   heardFrom,
-    //   rpInterest
-    // } = this.state;
+    const {
+      firstName,
+      lastName,
+      phoneNumber,
+      gender,
+      studentType,
+      major,
+      school,
+      transportation,
+      shirtSize,
+      diet,
+      graduationClass,
+      jobInterest,
+      professionalInterest,
+      heardFrom,
+      rpInterest
+    } = this.state;
     return (
       <div>
-        <Form>
-          <Row>
-            <Form.Label>First Name:</Form.Label>
-            <Form.Control type="text" placeholder="First name" />
-          </Row>
-          <Row>
-            <Form.Label>Last Name:</Form.Label>
-            <Form.Control type="text" placeholder="Last name" />
-          </Row>
-          <Row>
-            <Form.Label>Phone Number:</Form.Label>
-            <Form.Control type="number" placeholder="2173333426" />
-          </Row>
-          <Row>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Row>
-          <Row>
-            <Form.Label>Gender:</Form.Label>
-            <Form.Control type="text" placeholder="Gender" />
-          </Row>
-          <Row>
-            <Form.Label>Year in School:</Form.Label>
-            <Form.Control type="text" placeholder="Freshman" />
-          </Row>
-          <Row>
-            <Form.Label>Major:</Form.Label>
-            <Form.Control type="text" placeholder="Computer Science" />
-          </Row>
-          <Row>
-            <Form.Label>School:</Form.Label>
-            <Form.Control type="text" placeholder="UIUC" />
-          </Row>
-          <Row>
-            <Form.Label>Transportation:</Form.Label>
-            <Form.Control type="text" placeholder="Transportation" />
-          </Row>
-          <Row>
-            <Form.Label>Shirt Size:</Form.Label>
-            <Form.Control type="text" placeholder="Large" />
-          </Row>
-          <Row>
-            <Form.Label>Dietary Restriction:</Form.Label>
-            <Form.Control type="text" placeholder="Vegan" />
-          </Row>
-          <Row>
-            <Form.Label>Graduation Class:</Form.Label>
-            <Form.Control type="text" placeholder="2023 Fall" />
-          </Row>
-          <Row>
-            <Form.Label>Job Interest:</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Looking for Summer Internship"
-            />
-          </Row>
-          <Row>
-            <Form.Label>Proffesional Interest:</Form.Label>
-            <Form.Control type="text" placeholder="ML, AI and Big Data" />
-          </Row>
-          <Row>
-            <Form.Label>Heard From:</Form.Label>
-            <Form.Control type="text" placeholder="R|P reached out to me" />
-          </Row>
-          <Row>
-            <Form.Label>R|P Interest:</Form.Label>
-            <Form.Control type="text" placeholder="Very(????)" />
-          </Row>
-        </Form>
+        <form>
+          <label>
+            First Name:
+            <input type="text" name="firstName" />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" name="lastName" />
+          </label>
+          <label>
+            Phone Number:
+            <input type="number" name="phoneNumber" />
+          </label>
+          <label>
+            Gender:
+            <input type="text" name="gender" />
+          </label>
+          <label>
+            Year in School:
+            <input type="text" name="studentType" />
+          </label>
+          <label>
+            Major:
+            <input type="text" name="major" />
+          </label>
+          <label>
+            School:
+            <input type="text" name="school" />
+          </label>
+          <label>
+            Transportation:
+            <input type="text" name="transportation" />
+          </label>
+          <label>
+            Shirt Size:
+            <input type="text" name="shirtSize" />
+          </label>
+          <label>
+            Dietary Restriction:
+            <input type="text" name="diet" />
+          </label>
+          <label>
+            Graduation Class:
+            <input type="text" name="graduationClass" />
+          </label>
+          <label>
+            Job Interest:
+            <input type="text" name="jobInterest" />
+          </label>
+          <label>
+            Proffesional Interest:
+            <input type="text" name="professionalInterest" />
+          </label>
+          <label>
+            Heard From:
+            <input type="text" name="heardFrom" />
+          </label>
+          <label>
+            R|P interest:
+            <input type="text" name="rpInterest" />
+          </label>
+        </form>
       </div>
     );
   }
