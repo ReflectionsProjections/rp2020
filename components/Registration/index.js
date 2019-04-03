4ce588aae0f75b050eb81abbab25e3c8b33f9d8b
Rishub Podar <rishubpodar@wirelessprv-10-193-63-166.near.illinois.edu>
2019-04-03 11:21:33 -0500

    Started creating registration page

---
+++ components/Registration/index.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import './Registration.scss';
+
+class Registration extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    gender: '',
+    studentType: '',
+    major: '',
+    school: '',
+    transportation: '',
+    shirtSize: '',
+    diet: '',
+    graduationClass: '',
+    jobInterest: '',
+    professionalInterest: '',
+    heardFrom: '',
+    rpInterest: ''
+  };
+
+  componentDidMount() {
+    return () => {};
+  }
+
+  render() {
+    const {
+      firstName,
+      lastName,
+      phoneNumber,
+      gender,
+      studentType,
+      major,
+      school,
+      transportation,
+      shirtSize,
+      diet,
+      graduationClass,
+      jobInterest,
+      professionalInterest,
+      heardFrom,
+      rpInterest
+    } = this.state;
+    return (
+      <div>
+        <form>
+          <label>
+            First Name:
+            <input type="text" name="firstName" />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" name="lastName" />
+          </label>
+          <label>
+            Phone Number:
+            <input type="number" name="phoneNumber" />
+          </label>
+          <label>
+            Gender:
+            <input type="text" name="gender" />
+          </label>
+          <label>
+            Year in School:
+            <input type="text" name="studentType" />
+          </label>
+          <label>
+            Major:
+            <input type="text" name="major" />
+          </label>
+          <label>
+            School:
+            <input type="text" name="school" />
+          </label>
+          <label>
+            Transportation:
+            <input type="text" name="transportation" />
+          </label>
+          <label>
+            Shirt Size:
+            <input type="text" name="shirtSize" />
+          </label>
+          <label>
+            Dietary Restriction:
+            <input type="text" name="diet" />
+          </label>
+          <label>
+            Graduation Class:
+            <input type="text" name="graduationClass" />
+          </label>
+          <label>
+            Job Interest:
+            <input type="text" name="jobInterest" />
+          </label>
+          <label>
+            Proffesional Interest:
+            <input type="text" name="professionalInterest" />
+          </label>
+          <label>
+            Heard From:
+            <input type="text" name="heardFrom" />
+          </label>
+          <label>
+            R|P interest:
+            <input type="text" name="rpInterest" />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Registration;
