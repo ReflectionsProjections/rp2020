b1c8ca6288f78210d768b8b9bed186fc62b87eb1
divyah3 <divyah3@illinois.edu>
2019-04-22 16:41:10 -0500

    List by tier.

--- components/SponsorSection/index.js
+++ components/SponsorSection/index.js
@@ -6,6 +6,69 @@ import Col from 'react-bootstrap/Col';
 import axios from 'axios';
 
 import './SponsorSection.scss';
+
+
+const ImageViewer = (props) => {
+  const { size, images } = props;
+  console.log(props);
+  return (
+    <Container>
+      {images.map(image => {
+        return <Image src={image.image_url} width = {`${size}%`} fluid />
+      })}
+
+    </Container>
+  )
+
+}
+
+class SponsorSection extends Component {
+  state = {
+    sponsors: []
+  };
+
+  componentDidMount() {
+    axios.get('/static/data/sponsor.json').then(res => {
+      const { sponsors } = res.data;
+      this.setState({ sponsors });
+    });
+  }
+
+  render() {
+    const { sponsors } = this.state;
+    const tierMB = sponsors.filter(e => e.tier == "mb");
+    const tierGB = sponsors.filter(e => e.tier == "gb");
+    const tierTB = sponsors.filter(e => e.tier == "tb");
+    const tierPB = sponsors.filter(e => e.tier == "pb");
+
+    return (
+      <Container className="sponsor-section">
+        <div className="container">
+          <ImageViewer images = {tierPB} size = {100}></ImageViewer>
+          <ImageViewer images = {tierTB} size = {80}></ImageViewer>
+          <ImageViewer images = {tierGB} size = {60}></ImageViewer>
+          <ImageViewer images = {tierMB} size = {40}></ImageViewer>
+        </div>
+
+
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">One of three columns</div>
+            <div className="col-sm">One of three columns</div>
+            <div className="col-sm">One of three columns</div>
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default SponsorSection;
+
+
+
+
+
 // figure out how to loop through and display images
 
 // const SponsorSection = () => (
@@ -32,53 +95,3 @@ res = {
 }
 
 */
-class SponsorSection extends Component {
-  state = {
-    sponsors: []
-  };
-
-  componentDidMount() {
-    axios.get('/static/data/sponsor.json').then(res => {
-      const { sponsors } = res.data;
-      this.setState({ sponsors });
-    });
-  }
-
-  render() {
-    const { sponsors } = this.state;
-    return (
-      <Container className="sponsor-section">
-        {sponsors.map(sponsor => {
-          // if ()
-          return (
-            <div className="container">
-              <div className="row-2">
-                <div className="col-3">
-                  <Image src={sponsor.image_url} fluid />
-                </div>
-              </div>
-            </div>
-
-            // <Container>
-            //   <Row>
-            //     <Col xs={6} md={2}>
-            //       <Image src={sponsor.image_url} fluid />
-            //     </Col>
-            //   </Row>
-            // </Container>
-          );
-        })}
-
-        <div className="container">
-          <div className="row">
-            <div className="col-sm">One of three columns</div>
-            <div className="col-sm">One of three columns</div>
-            <div className="col-sm">One of three columns</div>
-          </div>
-        </div>
-      </Container>
-    );
-  }
-}
-
-export default SponsorSection;
