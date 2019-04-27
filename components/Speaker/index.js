3032bb1312f0932aafd73bdbe0237002b13ee058
Alpri Else <alprielse@gmail.com>
2019-04-27 17:47:37 -0500

    Redesign speaker section

--- components/Speaker/index.js
+++ components/Speaker/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import CardColumns from 'react-bootstrap/CardColumns';
 
@@ -22,42 +24,67 @@ class Speaker extends Component {
   render() {
     const { speakers } = this.state;
 
-    const cards = [];
-    speakers.forEach(data => {
-      const { name } = data;
-      const { image } = data;
-      const { biography } = data;
-      cards.push(
-        <Card>
-          <Card.Img variant="top" src={image} />
-          <Card.Body className="text-center">
-            <Card.Title>{name}</Card.Title>
-            <Card.Text>{biography}</Card.Text>
-          </Card.Body>
-        </Card>
-      );
-    });
-    if (cards.length === 0) {
-      cards.push(<Card />);
-      cards.push(
-        <Card>
-          <Card.Body>
-            <Card.Title className="text-center">Loading speakers...</Card.Title>
-          </Card.Body>
-        </Card>
-      );
-      cards.push(<Card />);
-    }
-
     return (
       <Container>
         <section className="speaker-section">
-          <h2 className="text-center section-header">Speakers</h2>
-          <div className="row mx-auto" />
-          <div>
-            <CardColumns>{cards}</CardColumns>
-          </div>
-          <br />
+          <Container>
+            <h2 className="text-center section-header">Speakers</h2>
+            <br />
+            <Row>
+              <Col>
+                <img
+                  src="/static/img/speakers/AlfredSpector.jpeg"
+                  className="rounded float-left"
+                  width="400em"
+                  alt="Two Sigma CTO Alfred Spector"
+                />
+              </Col>
+              <Col>
+                <h3>Keynote Speaker: Alfred Spector</h3>
+                <h4>CTO @ TwoSigma</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </Col>
+            </Row>
+            <br />
+            <hr />
+            <Row>
+              <div className="text-center">
+                {speakers.length === 0 ? (
+                  <h4>Loading...</h4>
+                ) : (
+                  <CardColumns>
+                    {speakers.map(speaker => {
+                      const { name } = speaker;
+                      const { image } = speaker;
+                      const { tagline } = speaker;
+                      return (
+                        <Card>
+                          <Card.Img
+                            variant="top"
+                            className="rounded"
+                            src={image}
+                          />
+                          <Card.Body className="text-center">
+                            <Card.Title>{name}</Card.Title>
+                            <Card.Text>{tagline}</Card.Text>
+                          </Card.Body>
+                        </Card>
+                      );
+                    })}
+                  </CardColumns>
+                )}
+              </div>
+            </Row>
+          </Container>
         </section>
       </Container>
     );
