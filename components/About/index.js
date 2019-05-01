bcf2085f9dee4d3f0f3e8497016aa6955e3d35cc
Alpri Else <alprielse@gmail.com>
2019-05-01 14:11:10 -0500

    Update copy and team photo

--- components/About/index.js
+++ components/About/index.js
@@ -33,8 +33,8 @@ const About = () => (
               <h3>Our Team</h3>
               <br />
               <p>
-                We are students passionate about technology who emobodies the
-                diversity and excellence inherit to the University of Illinois
+                We are students passionate about technology who embody the
+                diversity and excellence inherit at the University of Illinois
                 at a Urbana-Champaign. We bring industry and academia into one
                 conference to incite the spread of novel ideas. We connect
                 students with professional opportunities. We host a one of a
