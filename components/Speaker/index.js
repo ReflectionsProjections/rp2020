47eef3ed4d40831a8b231ca8bdda78a5e4d8e056
Alpri Else <alprielse@gmail.com>
2019-06-17 19:44:56 -0500

    speaker-section hover animations

--- components/Speaker/index.js
+++ components/Speaker/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 
@@ -15,33 +14,6 @@ const Speaker = ({ speakers }) => (
         <h2 className="text-center section-header">Speakers</h2>
         <br />
         <Row>
-          <Col md={5}>
-            <img
-              src="/static/img/speakers/AlfredSpector.jpeg"
-              className="rounded float-left mb-5"
-              width="100%"
-              alt="Two Sigma CTO Alfred Spector"
-            />
-          </Col>
-          <Col md={7}>
-            <h3>Keynote Speaker: Alfred Spector</h3>
-            <h4>CTO and Head of Engineering @ TwoSigma</h4>
-            <p>
-              Alfred Spector is the current Chief Technology Officer and Head of
-              Engineering at TwoSigma, an investment management company that
-              leverages Machine Learning and Distributed Computing. Previously,
-              he was the Vice President of Research and Special Initiatives at
-              Google and, before that, the Vice President of Strategy &
-              Technology at IBM. Alfred recieved his PhD in Computer Science at
-              Stanford University and attended Harvard University for his
-              undergraduate studies. We are glad to welcome Alfred as our
-              keynote speaker for RP 2019!
-            </p>
-          </Col>
-        </Row>
-        <br />
-        <hr />
-        <Row>
           <div>
             {speakers.length === 0 ? (
               <h4>Loading...</h4>
@@ -53,10 +25,18 @@ const Speaker = ({ speakers }) => (
                   const { tagline } = speaker;
                   return (
                     <Card className={styles.speakerCard} key={name}>
-                      <Card.Img variant="top" src={image} />
+                      <Card.Img
+                        variant="top"
+                        src={image}
+                        className={styles.cardImage}
+                      />
                       <Card.Body>
-                        <Card.Title>{name}</Card.Title>
-                        <Card.Text>{tagline}</Card.Text>
+                        <Card.Title className={styles.speakerName}>
+                          {name}
+                        </Card.Title>
+                        <Card.Text className={styles.speakerTagline}>
+                          {tagline}
+                        </Card.Text>
                       </Card.Body>
                     </Card>
                   );
