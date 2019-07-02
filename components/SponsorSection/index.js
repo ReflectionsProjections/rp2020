defea733f632d3fa356c2ce993df2c0bc09e2c8d
divyah3 <divyah3@illinois.edu>
2019-07-02 14:07:53 -0700

    Using rows and columns.

--- components/SponsorSection/index.js
+++ components/SponsorSection/index.js
@@ -4,18 +4,49 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import axios from 'axios';
-
 import './SponsorSection.scss';
 
 const ImageViewer = props => {
-  const { size, images } = props;
+  const { id, images } = props;
   console.log(props);
+
+  let xs = 12;
+  let sm = 12;
+  let md = 12;
+  let lg = 12;
+
+  if (id == 'petab') {
+    xs = 12;
+    sm = 12;
+    md = 12;
+    lg = 12;
+  } else if (id == 'terab') {
+    xs = 6;
+    sm = 6;
+    md = 6;
+    lg = 4;
+  } else if (id == 'gigab') {
+    xs = 6;
+    sm = 4;
+    md = 3;
+    lg = 4;
+  } else if (id == 'megab') {
+    xs = 4;
+    sm = 3;
+    md = 3;
+    lg = 3;
+  }
+
   return (
-    <Container>
+    <Row>
       {images.map(image => {
-        return <Image src={image.img} width={`${size}%`} fluid />;
+        return (
+          <Col xs={xs} sm={sm} md={md} lg={lg}>
+            <Image src={image.img} fluid />
+          </Col>
+        );
       })}
-    </Container>
+    </Row>
   );
 };
 
@@ -27,19 +58,17 @@ class SponsorSection extends Component {
 
   componentDidMount() {
     axios.get('/static/data/sponsor.json').then(res => {
-      this.setState({ sponsors : res.data.sponsors, loaded: true });
+      this.setState({ sponsors: res.data.sponsors, loaded: true });
     });
   }
 
   render() {
     const { sponsors } = this.state;
-
     return (
       <Container className="sponsor-section">
         <div className="container">
           <h2 className="text-center section-header">Sponsors</h2>
           <div id="sponsor-tier" className="col-md-6 offset-md-3 text-center">
-            
             {!this.state.loaded ? (
               <h1>Loading</h1>
             ) : (
@@ -56,21 +85,20 @@ class SponsorSection extends Component {
 
                 <div id="tera-tier">
                   <h3>Terabyte</h3>
-                  <ImageViewer images={sponsors.tier2} size={50} />
+                  <ImageViewer images={sponsors.tier2} id="terab" size={50} />
                 </div>
 
                 <div id="gig-tier">
                   <h3>Gigabyte</h3>
-                  <ImageViewer images={sponsors.tier3} size={40} />
+                  <ImageViewer images={sponsors.tier3} id="gigab" size={40} />
                 </div>
 
                 <div id="mega-tier">
                   <h3>Megabyte</h3>
-                  <ImageViewer images={sponsors.tier4} size={30} />
-                </div> 
-            </div>
-            )
-          }
+                  <ImageViewer images={sponsors.tier4} id="megab" size={30} />
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </Container>
