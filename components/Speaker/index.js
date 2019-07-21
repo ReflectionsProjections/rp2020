f5790fedf7a4f418c844e1372f3fefbd76818f96
Alpri Else <alprielse@gmail.com>
2019-07-21 12:29:10 -0400

    Add speaker info

--- components/Speaker/index.js
+++ components/Speaker/index.js
@@ -1,60 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
 import Section from '../Util/Section';
 
 import styles from './Speaker.scss';
 
-const Speaker = ({ speakers }) => (
-  <Section className="speaker-section">
-    <Section.Header>
-      <Section.Title>Speakers</Section.Title>
-    </Section.Header>
-    <Section.Body>
-      <Container>
-        <Row>
-          <div>
+const Speaker = ({ speakers }) => {
+  const [hoveredSpeaker, setHoveredSpeaker] = useState('');
+
+  const handleMouseHover = current => {
+    return () => {
+      setHoveredSpeaker(current);
+    };
+  };
+
+  return (
+    <Section className="speaker-section">
+      <Section.Header>
+        <Section.Title>Speakers</Section.Title>
+      </Section.Header>
+      <Section.Body>
+        <Container>
+          <Row>
             {speakers.length === 0 ? (
               <h4>Loading...</h4>
             ) : (
-              <CardDeck>
+              <>
                 {speakers.map(speaker => {
                   const { name, image, tagline } = speaker;
+                  const regularImageURL = `${image}.png`;
+                  const cutoutImageURL = `${image}-Cutout.png`;
                   return (
-                    <Link
-                      key={name}
-                      href={`/speaker?name=${name}`}
-                      as={`/speaker/${name}`}
+                    <Col
+                      className={styles.speakerCardContainer}
+                      xs={6}
+                      md={4}
+                      lg={3}
                     >
-                      <Card className={styles.speakerCard} key={name}>
-                        <Card.Img
-                          variant="top"
-                          src={image}
-                          className={styles.cardImage}
-                        />
-                        <Card.Body>
-                          <Card.Title className={styles.speakerName}>
-                            {name}
-                          </Card.Title>
-                          <Card.Text className={styles.speakerTagline}>
-                            {tagline}
-                          </Card.Text>
-                        </Card.Body>
-                      </Card>
-                    </Link>
+                      <Link
+                        key={name}
+                        href={`/speaker?name=${name}`}
+                        as={`/speaker/${name}`}
+                      >
+                        <Card className={styles.speakerCard} key={name}>
+                          <Card.Img
+                            variant="top"
+                            src={
+                              hoveredSpeaker === name
+                                ? cutoutImageURL
+                                : regularImageURL
+                            }
+                            className={styles.cardImage}
+                            onMouseEnter={handleMouseHover(name)}
+                            onMouseLeave={handleMouseHover(name)}
+                          />
+                          <Card.Body>
+                            <Card.Title className={styles.speakerName}>
+                              {name}
+                            </Card.Title>
+                            <Card.Text className={styles.speakerTagline}>
+                              {tagline}
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Link>
+                    </Col>
                   );
                 })}
-              </CardDeck>
+              </>
             )}
-          </div>
-        </Row>
-      </Container>
-    </Section.Body>
-  </Section>
-);
+          </Row>
+        </Container>
+      </Section.Body>
+    </Section>
+  );
+};
 
 export default Speaker;
