93905cd37681ea7d28f77d0a7ec00fb308592780
Alpri Else <alprielse@gmail.com>
2019-09-14 22:36:17 -0500

    hail mary fix

--- api/client.js
+++ api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const prefix =
   process.env.NODE_ENV === 'production'
-    ? 'https://www.reflectionsprojections.org/static/data/rp2019.json'
+    ? 'https://www.reflectionsprojections.org'
     : 'http://localhost:3000';
 
 export const fetchConferenceData = async () => {
