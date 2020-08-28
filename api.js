e37e13934876cab22f365b2b818d252602b3b3dc
Shresta Bangaru <shrestab19@gmail.com>
2020-08-28 13:23:43 -0500

    Adding api methods

---
+++ api.js
@@ -0,0 +1,51 @@
+const API = 'https://api.hackillinois.org';
+
+function request(method, endpoint, body) {
+  return fetch(API + endpoint, {
+    method,
+    headers: {
+      Authorization: sessionStorage.getItem('token'),
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw Error(res);
+  });
+}
+
+export function isAuthenticated() {
+  return sessionStorage.getItem('token');
+}
+
+export function authenticate(to) {
+  if (process.env.REACT_APP_TOKEN) {
+    sessionStorage.setItem('token', process.env.REACT_APP_TOKEN);
+  } else {
+    to = `${process.env.REACT_APP_URL}/auth/?to=${to}`;
+    to = `${API}/auth/google/?redirect_uri=${to}`;
+  }
+  window.location.replace(to);
+}
+
+export function getToken(code) {
+  return request('POST', '/auth/code/github/', { code })
+    .then(res => res.token);
+}
+
+/*export function uploadFile(file, type) {
+  return request('GET', `/upload/${type}/upload/`)
+    .then(res => fetch(res[type], {
+      method: 'PUT',
+      headers: { 'Content-Type': file.type },
+      body: file,
+    }))
+    .then(res => {
+      if (res.ok) {
+        return res;
+      }
+      throw Error(res);
+    });
+}*/
